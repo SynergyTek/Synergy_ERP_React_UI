9e5d006b5994f6c7841b4e81efcce92171923ec5
Anjali Raghuwanshi <anjaliraghu67@gmail.com>
2025-05-23 18:23:08 +0530

    dashboard changes

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ module.exports = withBundleAnalyzer({
 		styledComponents: true,
 	},
 
+	images: {
+		domains: ['placehold.co', 'images.unsplash.com'],
+	},
+
 	webpack: (config, { isServer }) => {
 		config.resolve.alias['#outline'] = path.resolve(__dirname, 'node_modules/@awesome.me/kit-9b926a9ec0/icons/modules/classic/thin');
 		config.resolve.alias['#duotone'] = path.resolve(__dirname, 'node_modules/@awesome.me/kit-9b926a9ec0/icons/modules/duotone/solid');
